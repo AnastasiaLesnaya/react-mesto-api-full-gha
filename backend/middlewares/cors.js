5efafc8eebad18a4354e30574ff5faef3bd15690
Anastasia Lesnaya <lesnyasha@yandex.ru>
2023-07-18 02:36:24 +0300

    пр15

---
+++ backend/middlewares/cors.js
@@ -0,0 +1,34 @@
+// Массив доменов, с которых разрешены кросс-доменные запросы
+const allowedCors = [
+  'https://mestolesnoy.nomoredomains.work',
+  'http://mestolesnoy.nomoredomains.work',
+  'https://api.mestolesnoy.nomoredomains.work',
+  'http://api.mestolesnoy.nomoredomains.work',
+  'https://localhost:3000',
+  'http://localhost:3000',
+  'https://localhost:3001',
+  'http://localhost:3001',
+];
+
+module.exports = allowedCors;
+// eslint-disable-next-line consistent-return
+module.exports = (req, res, next) => {
+  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  // проверяем, что источник запроса есть среди разрешённых
+  if (allowedCors.includes(origin)) {
+    // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  // предварительный запрос
+  if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+  return next();
+};
