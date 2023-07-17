9e7cb9f763e85a27a1ede7b419016d7a09e6a8e9
Anastasia Lesnaya <lesnyasha@yandex.ru>
2023-07-18 01:26:49 +0300

    пр15

--- backend/app.js
+++ backend/app.js
@@ -2,30 +2,21 @@ require('dotenv').config();
 // eslint-disable-next-line import/no-extraneous-dependencies
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const { errors } = require('celebrate');
+
+const { PORT = 3000, MONGODB = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+
 // Защита сервера
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
-// const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
 
-const { errors } = require('celebrate');
-const responseHandler = require('./middlewares/res-handler');
-const { requestLogger, errorLogger } = require('./middlewares/logger');
-const userRouter = require('./routes/users');
-const cardRouter = require('./routes/cards');
-const NotFound = require('./utils/errors/NotFound');
-
-const { PORT = 3000 } = process.env;
+const cors = require('./middlewares/cors');
+// Роуты
+const mainRouter = require('./routes/index');
 
 const app = express();
-app.use(bodyParser.json());
-app.use(cookieParser());
-
-// app.use(cors());
-
-const MONGODB = 'mongodb://127.0.0.1:27017/mestodb';
-mongoose.connect(MONGODB);
 
 // https://www.npmjs.com/package/express-rate-limit
 // Для ограничения кол-ва запросов. Для защиты от DoS-атак.
@@ -34,67 +25,22 @@ const limiter = rateLimit({
   max: 100,
 });
 
+const responseHandler = require('./middlewares/res-handler');
+
+mongoose.connect(MONGODB);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(limiter);
 app.use(helmet());
+app.use(cookieParser());
+app.use(cors);
 
-app.use(requestLogger);
+app.use(mainRouter);
 
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
-
-// Массив доменов, с которых разрешены кросс-доменные запросы
-const allowedCors = [
-  'http://localhost:3000',
-  'https://mestolesnoy.nomoredomains.work',
-  'http://mestolesnoy.nomoredomains.work',
-  'https://api.mestolesnoy.nomoredomains.work',
-  'http://api.mestolesnoy.nomoredomains.work/',
-];
-
-// безопасность
-app.use((req, res, next) => {
-  // Сохраняем источник запроса в переменную origin и
-  // проверяем, что источник запроса есть среди разрешённых
-  const { origin } = req.headers;
-  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
-  const requestHeaders = req.headers['access-control-request-headers'];
-  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  if (allowedCors.includes(origin)) {
-    // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
-  }
-  // Если это предварительный запрос, добавляем нужные заголовки
-  if (method === 'OPTIONS') {
-    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    // завершаем обработку запроса и возвращаем результат клиенту
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    // завершаем обработку запроса и возвращаем результат клиенту
-    res.end();
-    return;
-  }
-  next();
-});
-
-app.use('/', userRouter);
-app.use('/', cardRouter);
-
-app.use((req, res, next) => {
-  next(new NotFound('Страница не найдена'));
-});
-
-app.use(errorLogger);
-// celebrate
 app.use(errors());
-// 500
 app.use(responseHandler);
 
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
   console.log(`App listening on port ${PORT}`);
 });
