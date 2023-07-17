256e3b578d7735df42a7e73334605f09ce794c2f
Anastasia Lesnaya <lesnyasha@yandex.ru>
2023-07-18 02:07:57 +0300

    пр15

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,6 @@ const { PORT = 3001, MONGODB = 'mongodb://127.0.0.1:27017/mestodb' } = process.e
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 
-const cors = require('./middlewares/cors');
 // Роуты
 const mainRouter = require('./routes/index');
 
@@ -34,7 +33,44 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(limiter);
 app.use(helmet());
 app.use(cookieParser());
-app.use(cors);
+
+// Массив доменов, с которых разрешены кросс-доменные запросы
+const allowedCors = [
+  'https://mesto.place.nomoredomains.work',
+  'http://mesto.place.nomoredomains.work',
+  'https://api.mesto.place.nomoredomains.work',
+  'http://api.mesto.place.nomoredomains.work',
+  'https://localhost:3000',
+  'http://localhost:3000',
+  'https://localhost:3001',
+  'http://localhost:3001',
+];
+
+app.use((req, res, next) => {
+  // Сохраняем источник запроса в переменную origin и
+  // проверяем, что источник запроса есть среди разрешённых
+  const { origin } = req.headers;
+  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+  const requestHeaders = req.headers['access-control-request-headers'];
+  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  if (allowedCors.includes(origin)) {
+    // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+  }
+  // Если это предварительный запрос, добавляем нужные заголовки
+  if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    // завершаем обработку запроса и возвращаем результат клиенту
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    // завершаем обработку запроса и возвращаем результат клиенту
+    res.end();
+    return;
+  }
+  next();
+});
 
 app.use(mainRouter);
 
