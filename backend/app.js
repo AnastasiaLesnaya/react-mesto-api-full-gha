d04f44713cf2e176bf538f5dabf19681eebd94f7
Anastasia Lesnaya <lesnyasha@yandex.ru>
2023-07-20 00:57:45 +0300

    пр15

--- backend/app.js
+++ backend/app.js
@@ -2,43 +2,40 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const { errors } = require('celebrate');
-
-const { PORT = 3000 } = process.env;
-
-const cors = require('cors');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 // Защита сервера
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
+const { errors } = require('celebrate');
+const responseHandler = require('./middlewares/res-handler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const { ALLOWED_CORS } = require('./middlewares/cors');
+const cors = require('./middlewares/cors');
 
 // Роуты
 const mainRouter = require('./routes/index');
 
+const { PORT = 3000 } = process.env;
+
 const app = express();
-app.use(cors(ALLOWED_CORS));
+
+const MONGODB = 'mongodb://127.0.0.1:27017/mestodb';
+mongoose.connect(MONGODB);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
 app.use(cookieParser());
-
+app.use(requestLogger);
+app.use(cors);
 // https://www.npmjs.com/package/express-rate-limit
 // Для ограничения кол-ва запросов. Для защиты от DoS-атак.
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
 });
-const responseHandler = require('./middlewares/res-handler');
-
-const MONGODB = 'mongodb://127.0.0.1:27017/mestodb';
-mongoose.connect(MONGODB);
 
 app.use(limiter);
-app.use(helmet());
-app.use(requestLogger);
 
 app.get('/crash-test', () => {
   setTimeout(() => {
